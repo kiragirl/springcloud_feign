ccaa99b44098d87e550d8e46e69235746d1d8f56
liyiming <liyiming0215@sina.com>
2018-07-12 14:15:13 +0800

    添加熔断器功能

--- src/main/java/com/liyiming/springcloud/feign/service/HelloService.java
+++ src/main/java/com/liyiming/springcloud/feign/service/HelloService.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.liyiming.springcloud.feign.hystric.HelloServiceHystric;
+
 /**
  * <p>
  * Title: HelloService
@@ -23,8 +25,15 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author liyiming
  * @date 2018年4月12日
  */
-@FeignClient(value = "service-hi")
 @Service
+/**
+ * 
+ * <p>Title: HelloService</p>  
+ * <p>Description: 当发生熔断时 触发 HelloServiceHystric.sayHiFromClientOne</p>  
+ * @author liyiming  
+ * @date 2018年7月12日
+ */
+@FeignClient(value = "service-hi",fallback = HelloServiceHystric.class)
 public interface HelloService {
 	@RequestMapping(value = "/hi", method = RequestMethod.GET)
 	String sayHiFromClientOne(@RequestParam(value = "name") String name);
