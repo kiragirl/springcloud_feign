20a5d4ac7299d75e5fce4fb90796ee8403845628
liyiming <liyiming0215@sina.com>
2018-07-12 15:56:27 +0800

    dashboard

--- src/main/java/com/liyiming/springcloud/feign/SpringcloudFeignApplication.java
+++ src/main/java/com/liyiming/springcloud/feign/SpringcloudFeignApplication.java
@@ -3,10 +3,13 @@ package com.liyiming.springcloud.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrixDashboard
 public class SpringcloudFeignApplication {
 
 	public static void main(String[] args) {
