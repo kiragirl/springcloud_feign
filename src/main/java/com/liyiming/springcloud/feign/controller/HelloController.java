88578744ebaa26b9c4530559808d78258031b898
liyiming <liyiming0215@sina.com>
2018-07-18 16:34:57 +0800

    服务链路追踪(Spring Cloud Sleuth) ribbon作为负载均衡可以追踪到，feign却不能

--- src/main/java/com/liyiming/springcloud/feign/controller/HelloController.java
+++ src/main/java/com/liyiming/springcloud/feign/controller/HelloController.java
@@ -6,6 +6,8 @@
  */
 package com.liyiming.springcloud.feign.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,21 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.liyiming.springcloud.feign.service.HelloService;
 
-
-/**  
- * <p>Title: HelloController</p>  
- * <p>Description: </p>  
- * @author liyiming  
- * @date 2018年4月12日  
+/**
+ * <p>
+ * Title: HelloController
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * @author liyiming
+ * @date 2018年4月12日
  */
 @RestController
 public class HelloController {
-
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private HelloService helloService;
-	
-    @RequestMapping("/hi")
-    public String home(@RequestParam String name) {
-        return helloService.sayHiFromClientOne(name);
-    }
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name) {
+		log.info("feign is being calling ");
+		return helloService.sayHiFromClientOne(name);
+	}
 }
